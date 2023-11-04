d9bb273ab9de998115df668044e70f402ae00fb7
kalineid <noob@mail.ustc.edu.cn>
2023-11-04 06:01:26 +0000

    Support multi client

--- examples/web_demo.py
+++ examples/web_demo.py
@@ -22,18 +22,20 @@ parser.add_argument('--model_name', type=str, default="fastertransformer",
                     help='model_name')
 FLAGS = parser.parse_args()
 
-tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir="/data/checkpoint_hub")
+tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-13b-chat-hf", cache_dir="/data/checkpoint_hub")
 tokenizer.pad_token_id = 0
 tokenizer.padding_side = "left"
 
 verbose = 0
 
 
-def lines2inputs(FLAGS, lines, tokenizer, max_length):
+def lines2inputs(FLAGS, lines, tokenizer, max_length, top_p, temperature):
     encoded_inputs = tokenizer.batch_encode_plus(lines)
     input_ids = np.array(tokenizer.pad(encoded_inputs, pad_to_multiple_of=16)["input_ids"], dtype=np.uint32)
     input_lengths = np.array([len(ids) for ids in input_ids], dtype=np.uint32).reshape(-1, 1)
     request_output_len = max_length * np.ones([input_ids.shape[0], 1]).astype(np.uint32)
+    runtime_top_p = top_p * np.ones([input_ids.shape[0], 1]).astype(np.float32)
+    temperature = temperature * np.ones([input_ids.shape[0], 1]).astype(np.float32)
 
     def to_input(name, np_input):
         client_util = httpclient if FLAGS.protocol == "http" else grpcclient
@@ -44,7 +46,9 @@ def lines2inputs(FLAGS, lines, tokenizer, max_length):
 
     inputs = [to_input("input_ids", input_ids),
               to_input("input_lengths", input_lengths),
-              to_input("request_output_len", request_output_len)]
+              to_input("request_output_len", request_output_len),
+              to_input("runtime_top_p", runtime_top_p),
+              to_input("temperature", temperature)]
     return inputs, input_lengths
 
 
@@ -96,33 +100,18 @@ def create_inference_server_client(protocol, url, concurrency, verbose):
     client_util = httpclient if protocol == "http" else grpcclient
     if protocol == "http":
         return client_util.InferenceServerClient(url,
-                                                concurrency=concurrency,
-                                                verbose=verbose)
+                                                 concurrency=concurrency,
+                                                 verbose=verbose)
     elif protocol == "grpc":
         return client_util.InferenceServerClient(url,
-                                                verbose=verbose)
+                                                 verbose=verbose)
 
 
-def predict(client, input, chatbot, max_length, top_p, temperature):
+def predict(input, chatbot, max_length, top_p, temperature):
     chatbot.append((parse_text(input), ""))
-    inputs, input_lengths = lines2inputs(FLAGS, [input], tokenizer, max_length)
-    client.async_stream_infer(FLAGS.model_name, inputs)
 
-    while True:
-        try:
-            result = result_queue.get(timeout=0.5)
-        except Empty:
-            break
-
-        if type(result) == InferenceServerException:
-            raise result
-
-        seq_len = result.as_numpy("sequence_length")[0, 0]
-        output_ids = result.as_numpy("output_ids")[0][0][input_lengths[0][0]+1:seq_len]
-        token_text = tokenizer.decode(output_ids)
-
+    for token_text in model.stream_chat(input, max_length, top_p, temperature):
         chatbot[-1] = (parse_text(input), parse_text(token_text))
-
         yield chatbot
 
 
@@ -131,15 +120,36 @@ def reset_user_input():
 
 
 def reset_state():
-    return [], []
+    return []
 
 
-result_queue = Queue()
-client = create_inference_server_client(FLAGS.protocol,
-                                        FLAGS.url,
-                                        concurrency=1,
-                                        verbose=verbose)
-client.start_stream(callback=partial(stream_callback, result_queue))
+class TritongRPCModel:
+    def stream_chat(self, input, max_length, top_p, temperature):
+        with create_inference_server_client(FLAGS.protocol,
+                                            FLAGS.url,
+                                            concurrency=1,
+                                            verbose=verbose) as client:
+            result_queue = Queue()
+            client.start_stream(callback=partial(stream_callback, result_queue))
+            inputs, input_lengths = lines2inputs(FLAGS, [input], tokenizer, max_length, top_p, temperature)
+            client.async_stream_infer(FLAGS.model_name, inputs)
+
+            while True:
+                try:
+                    result = result_queue.get(timeout=2)
+                except Empty:
+                    break
+
+                if type(result) == InferenceServerException:
+                    raise result
+
+                seq_len = result.as_numpy("sequence_length")[0, 0]
+                output_ids = result.as_numpy("output_ids")[0][0][input_lengths[0][0]+1:seq_len]
+                token_text = tokenizer.decode(output_ids)
+                yield token_text
+
+
+model = TritongRPCModel()
 
 with gr.Blocks() as demo:
     gr.HTML("""<h1 align="center">ChatGLM</h1>""")
@@ -155,14 +165,13 @@ with gr.Blocks() as demo:
         with gr.Column(scale=1):
             emptyBtn = gr.Button("Clear History")
             max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
-            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
-            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.6, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.9, step=0.01, label="Temperature", interactive=True)
 
-    submitBtn.click(partial(predict, client), [user_input, chatbot, max_length, top_p, temperature], [chatbot],
+    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature], [chatbot],
                     show_progress=True)
     submitBtn.click(reset_user_input, [], [user_input])
 
     emptyBtn.click(reset_state, outputs=[chatbot], show_progress=True)
 
 demo.queue().launch(share=False, inbrowser=True)
-client.close()
